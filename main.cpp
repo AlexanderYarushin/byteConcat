d4edd5a4ec57364d121e7804745a04f17d12af6b
AlexanderYarushin <sasha-yarushin@mail.ru>
2021-02-05 23:42:05 +0500

    Released concatenate files

--- main.cpp
+++ main.cpp
@@ -1,63 +1,81 @@
-#include <iostream>
 #include <fstream>
-#include <filesystem>
+#include <iostream>
 
 using namespace std;
 
-void addToStorage(const char* fileName, uint8_t* storage);
-void sizeCalc(const char* fileName,unsigned &sizeAllFiles);
+static const char* outputFile = "output.concat";
+static const char* separate = ";";
 
-
+void writeOutput(const char* fileName, unsigned size);
+unsigned fileSize(const char* filename);
+void checkFile(const char* fileName);
+void split(const char* fileName);
 
 int main(int argc, char** argv)
 {
-    unsigned sizeAllFiles = 0;
+    //    for(int i = 1; i < argc; ++i){
+    //        char* fileName = argv[i];
+    //        unsigned  size = fileSize(fileName);
+    //        cout << size << endl;
+    //        writeOutput(fileName, size);
+    //    }
 
-    for(int i = 1; i < argc; ++i){
-        char* fileName = argv[i];
-        sizeCalc(fileName, sizeAllFiles);
-
-
-    }
-
-    cout << "Full size: " << sizeAllFiles << " byte" << endl;
-
-    uint8_t storage[sizeAllFiles];
-
-
-    for(int i = 1; i < argc; ++i){
-        char* fileName = argv[i];
-
-        addToStorage(fileName, storage);
-    }
-
+    char* fileName = argv[1];
+    split(fileName);
 
     return 0;
 }
 
-std::ifstream::pos_type fileSize(const char* filename)
+void split(const char* fileName)
+{
+    checkFile(fileName);
+
+    ifstream input(fileName, std::ios::in | std::ios::binary);
+}
+
+void checkFile(const char* fileName)
+{
+    std::fstream file(fileName);
+    if (!file.is_open()) {
+        cout << "Failed to open file: " << fileName << endl;
+        exit(1);
+    }
+}
+
+unsigned fileSize(const char* filename)
 {
     std::ifstream in(filename, std::ifstream::ate | std::ifstream::binary);
     return in.tellg();
 }
 
-void sizeCalc(const char* fileName, unsigned &sizeAllFiles){
-    ifstream input(fileName, std::ios::in | std::ios::binary);
-
-    if (!input.is_open()) {
-        cout << "Failed to open file: " << fileName  << endl;
-        return;
-    }
-
-    cout << "File size: " << fileSize(fileName) << " byte" << endl;
-    sizeAllFiles += fileSize(fileName);
-
+const char* intToCharP(unsigned x)
+{
+    std::string* s = new string;
+    *s = std::to_string(x);
+    return s->c_str();
 }
 
-void addToStorage(const char* fileName, uint8_t* storage){
+void writeOutput(const char* fileName, unsigned size)
+{
+
+    checkFile(fileName);
+
     ifstream input(fileName, std::ios::in | std::ios::binary);
-    if (!input.is_open()) {
-        cout << "Failed to open file: " << fileName << endl;
-        return;
+    static ofstream output(outputFile, std::ios::out | std::ios::binary);
+
+    size_t inputNameLength = strlen(fileName);
+
+    output.write(fileName, inputNameLength);
+    output.write(separate, sizeof(char));
+    output.write(intToCharP(size), strlen(intToCharP(size)));
+    output.write(separate, sizeof(char));
+
+    char buffer;
+    while (!input.eof()) {
+        input.read(&buffer, sizeof(char));
+        output.write(&buffer, sizeof(char));
     }
+
+    for (int i = 0; i < 100; ++i)
+        output.write("X", sizeof(char));
 }
